d569f804a7337db638c2d987005cbb1388a37c51
José Alonso <mail@josealonso.dev>
2023-01-05 23:18:35 -0600

    Add Razor Runtime Compilation

--- Program.cs
+++ Program.cs
@@ -8,10 +8,18 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
     options.EnableSensitiveDataLogging(true);
 });
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute("controllers", "controllers/{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 SeedData.SeedDatabase(app);
 

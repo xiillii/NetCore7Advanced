900b6c65fbf3891a6ca410ceb7dc813edd518187
José Alonso <mail@josealonso.dev>
2023-01-06 22:00:34 -0600

    Blazor with code-behind and code only

---
+++ Blazor/CodeOnly.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+using NetCore7Advanced.Models;
+
+namespace NetCore7Advanced.Blazor
+{
+    public class CodeOnly : ComponentBase
+    {
+        [Inject] public DataContext Context { get; set; }
+
+        public IEnumerable<string> Names => Context.People.Select(p => p.Firstname);
+
+        public bool Ascending { get; set; } = false;
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            var data = Ascending ? Names.OrderBy(n => n) : Names.OrderByDescending(n => n);
+
+            builder.OpenElement(1, "button");
+            builder.AddAttribute(2, "class", "btn btn-primary mb-2");
+            builder.AddAttribute(3, "onclick",
+                EventCallback.Factory.Create<MouseEventArgs>(this, () => Ascending = !Ascending));
+            builder.AddContent(4, new MarkupString("Toggle"));
+
+            builder.CloseElement();
+
+            builder.OpenElement(5, "ul");
+            builder.AddAttribute(6, "class", "list-group");
+            foreach (var name in data)
+            {
+                builder.OpenElement(7, "li");
+                builder.AddAttribute(8, "class", "list-group-item");
+                builder.AddContent(9, new MarkupString(name));
+                builder.CloseElement();
+            }
+
+            builder.CloseElement();
+        }
+    }
+}
---
+++ Blazor/Split.Razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using NetCore7Advanced.Models;
+
+namespace NetCore7Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject] public DataContext Context { get; set; }
+
+        public IEnumerable<string> Names => Context.People.Select(p => p.Firstname);
+    }
+}

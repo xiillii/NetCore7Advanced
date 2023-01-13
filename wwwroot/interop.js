8061b987e56f8a4293c2bbbe4f0f0cf178911458
José Alonso <mail@josealonso.dev>
2023-01-13 01:03:42 -0600

    Invoking a JS function from a component

---
+++ wwwroot/interop.js
@@ -0,0 +1,12 @@
+﻿function addTableRows(colCount) {
+    let elem = document.querySelector("tbody");
+    let row = document.createElement("tr");
+    elem.append(row);
+
+    for (var i = 0; i < colCount; i++) {
+        let cell = document.createElement("td");
+        cell.innerText = "New elements";
+
+        row.append(cell);
+    }
+}
